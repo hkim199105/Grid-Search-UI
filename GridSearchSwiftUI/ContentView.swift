526677bd0177378c47d771f6c3d3e3f8a43c5cf7
hkim <hkim199105@gmail.com>
2020-11-29 19:37:44 +0900

    Initial Commit

---
+++ GridSearchSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridSearchSwiftUI
+//
+//  Created by Hakyoung Kim on 2020/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridSearchSwiftUI/GridSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchSwiftUIApp.swift
+//  GridSearchSwiftUI
+//
+//  Created by Hakyoung Kim on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
